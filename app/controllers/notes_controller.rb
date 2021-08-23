d55134ddc0ff85d466b93b9a03b310a12a245278
Elise Brown <elisemarie88@gmail.com>
2021-08-23 15:34:40 -0600

    updating the controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,10 +1,20 @@
 class NotesController < ApplicationController
+  #READ - all notes
+  
   def index
+    @notes = Note.all
+    render component: "Notes", props: {notes: @notes}
   end
 
+  #READ - single note
   def show
+    @note = Note.find(params[:id])
+    render component: "Note", props: {note: @note}
   end
 
+  #NEW - new page in memory, but not the data base, use this to create our new form.
   def new
+    @note = Note.new
+    render component: "NoteNew", props: {note: @note}
   end
 end
---
+++ app/javascript/components/Note.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Note = (props) => {
+  return (
+    <div>
+      <h1> {props.note.title}</h1>
+      
+    </div>
+  )
+}
